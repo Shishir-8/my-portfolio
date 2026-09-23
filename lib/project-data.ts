03730acb9e72a40a18d389d23834f738935d508e
Shishir Oli <shishir@Shishirs-MacBook-Air.local>
2026-09-23 08:52:30 +0545

    fix: image name

--- lib/project-data.ts
+++ lib/project-data.ts
@@ -4,7 +4,7 @@ export const projectData = [
     {
         id: 1,
         title: "Concrete Connections",
-        imageUrl: "/concrete.png",
+        imageUrl: "/concrete.jpg",
         link: "https://concreteconnections.com.au/",
         description: "Concrete Connections is dedicated to providing reliable platform for builders, concrete suppliers, and pumpers to connect and collaborate. We strive to simplify the process of sourcing and delivering concrete, ensuring timely and efficient service for all our users."
     },
@@ -13,7 +13,7 @@ export const projectData = [
        {
         id: 2,
         title: "Site IQ Pro",
-        imageUrl: "/siteiq.png",
+        imageUrl: "/siteiq.jpg",
         link: "https://siteiqpro.com.au/",
         description: "SiteIQPro is dedicated to providing reliable payroll management solutions for businesses of all sizes. We streamline your HR processes with our comprehensive platform."
     },
@@ -29,7 +29,7 @@ export const projectData = [
         id: 4,
         title: "ब SERA",
         link: "https://baserasolutions.com/",
-        imageUrl: "/basera.png",
+        imageUrl: "/basera.jpg",
         description:"Basera is a comfort-focused brand creating simple, ergonomic products for work, travel, and home. Our cushions, back supports, and pillows help reduce strain, improve posture, and make everyday life more comfortable.",
     }
 ]
